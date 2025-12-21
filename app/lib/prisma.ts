82d3cc82f14de1f2f1a5905f8f060bfe7ed36571
A A M Salehin Chowdhury <aamsalehinchowdhury@MacBookPro.broadband>
2025-12-21 13:47:45 +0000

    fixed prisma

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,24 +1,18 @@
-// app/lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../../app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
 }
 
-const pool = new Pool({
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-const adapter = new PrismaPg(pool)
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  })
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+export default prisma
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,26 @@
 // prisma.config.ts
+// import 'dotenv/config'
+// import { defineConfig, env } from 'prisma/config'
+
+// export default defineConfig({
+//   schema: 'prisma/schema.prisma',
+//   migrations: {
+//     path: 'prisma/migrations',
+//   },
+//   datasource: {
+//     url: env('DATABASE_URL'),
+//   },
+// })
+
 import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
-    path: 'prisma/migrations',
+    path: 'prisma/migrations'
   },
   datasource: {
     url: env('DATABASE_URL'),
   },
-})
+});
