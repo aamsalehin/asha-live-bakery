c43c3372cd93dca9d7b31e7794150c8582e8ed5a
A A M Salehin Chowdhury <aamsalehin@gmail.com>
2026-02-19 13:12:35 +0000

    api delted

--- app/api/admin/orders/[id]/route.ts
+++ app/api/admin/orders/[id]/route.ts
@@ -1,36 +1,36 @@
-import { NextResponse } from "next/server";
-import prisma from '@/lib/prisma'
+// import { NextResponse } from "next/server";
+// import prisma from '@/lib/prisma'
 
-export const runtime = "nodejs";
+// export const runtime = "nodejs";
 
-export async function PATCH(
-  _req: Request,
-  context: { params: Promise<{ id: string }> }
-) {
-  // ✅ UNWRAP params
-  const { id } = await context.params;
+// export async function PATCH(
+//   _req: Request,
+//   context: { params: Promise<{ id: string }> }
+// ) {
+//   // ✅ UNWRAP params
+//   const { id } = await context.params;
 
-  console.log("PATCH ORDER ID:", id);
+//   console.log("PATCH ORDER ID:", id);
 
-  if (!id) {
-    return NextResponse.json(
-      { error: "Missing order id" },
-      { status: 400 }
-    );
-  }
+//   if (!id) {
+//     return NextResponse.json(
+//       { error: "Missing order id" },
+//       { status: 400 }
+//     );
+//   }
 
-  try {
-    const order = await prisma.order.update({
-      where: { id },
-      data: { status: "received" },
-    });
+//   try {
+//     const order = await prisma.order.update({
+//       where: { id },
+//       data: { status: "received" },
+//     });
 
-    return NextResponse.json({ success: true, order });
-  } catch (error) {
-    console.error("PATCH ERROR:", error);
-    return NextResponse.json(
-      { error: "Failed to update order status" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ success: true, order });
+//   } catch (error) {
+//     console.error("PATCH ERROR:", error);
+//     return NextResponse.json(
+//       { error: "Failed to update order status" },
+//       { status: 500 }
+//     );
+//   }
+// }
